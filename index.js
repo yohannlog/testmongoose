efb1233b6fe531b1c11494eeead22de8637492f1
Alexandre <heynderickxalexandre@gmail.com>
2020-11-26 15:51:31 +0100

    CA FONCTIONNE !

--- index.js
+++ index.js
@@ -31,26 +31,28 @@ app.get('/', async (req, res) => {
 app.post('/upload', async (req, res) => {
 
 	let form = new formidable.IncomingForm();
-	form.uploadDir = "./img";
+	form.uploadDir = "./public/img";
 	form.keepExtensions = true;
 
 	form.parse(req, function (err, fields, files) {
-		res.writeHead(200, {'content-type': 'text/plain'});
-		res.write('received upload\n\n');
-
+		// res.writeHead(200, {'content-type': 'text/plain'});
+		// res.write('received upload\n\n');
 		console.log("form.bytesReceived");
+
 		console.log("file size: "+JSON.stringify(files.fileUploaded.size));
 		console.log("file path: "+JSON.stringify(files.fileUploaded.path));
 		console.log("file name: "+JSON.stringify(files.fileUploaded.name));
 		console.log("file type: "+JSON.stringify(files.fileUploaded.type));
 		console.log("astModifiedDate: "+JSON.stringify(files.fileUploaded.lastModifiedDate));
-		fs.rename(files.fileUploaded.path, './img/'+files.fileUploaded.name, function(err) {
+		fs.rename(files.fileUploaded.path, 'public/img/'+files.fileUploaded.name, function(err) {
 			if (err)
 				throw err;
-			console.log('renamed complete');
-
+			else{
+				console.log('renamed complete');
+				res.render("upload.ejs",{image:'./img/'+files.fileUploaded.name})
+			}
 		});
-		res.end();
+		// res.end();
 	});
 });
 
